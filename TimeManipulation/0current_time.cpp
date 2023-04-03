766ad9beb3201f09ccadd0beca0d1d722d8a7c50
longovinicius <longo.vinicius@gmail.com>
2023-04-03 20:45:00 -0300

    Adding first timing exercise

---
+++ TimeManipulation/0current_time.cpp
@@ -0,0 +1,19 @@
+#include <chrono>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    // Obtém o tempo atual
+    auto now = std::chrono::system_clock::now();
+
+    // Converte o tempo atual para um formato de tempo legível
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+
+    // Imprime o tempo atual
+    cout << "Tempo atual: " << ctime(&now_time_t);
+
+    cout << sizeof(now_time_t) << endl;
+
+    return 0;
+}
