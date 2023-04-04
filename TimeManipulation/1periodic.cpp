2f3b3233df19f56dcbde5ff18e4c51d710719d26
longovinicius <longo.vinicius@gmail.com>
2023-04-04 11:53:21 -0300

    thread espera tempo definido - tempo de execucao

--- TimeManipulation/1periodic.cpp
+++ TimeManipulation/1periodic.cpp
@@ -4,21 +4,25 @@
 
 using namespace std;
 
-void threadFunction()
-{
-    auto now = chrono::system_clock::now();                             // obtém o tempo atual
-    auto now_c = chrono::system_clock::to_time_t(now);                  // converte para o formato de tempo padrão
-    std::cout << "Thread executando às " << std::ctime(&now_c) << endl; // imprime a hora atual
+void threadFunction(int8_t period){
+    auto start = chrono::high_resolution_clock::now();
+    cout << "Thread executing" << endl;
+    auto end = chrono::high_resolution_clock::now();
+
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto period_mili = chrono::milliseconds(period*1000);
+    this_thread::sleep_for(period_mili - duration);
 }
 
 int main()
 {   
-    auto now = chrono::system_clock::now();
-    auto now_format = chrono::system_clock::to_time_t(now);
+    auto now_time = chrono::system_clock::now();
+    auto now_format = chrono::system_clock::to_time_t(now_time);
     cout << "Programa comecou às " << ctime(&now_format) << endl;
-    this_thread::sleep_for(5s); 
-    thread t(threadFunction); // cria uma nova thread para executar a função
+    thread t(threadFunction, 5); // cria uma nova thread para executar a função
     t.join();                      
-
+    auto now_time_end = chrono::system_clock::now();
+    auto now_format_end = chrono::system_clock::to_time_t(now_time_end);
+    cout << "Programa finalizou às " << ctime(&now_format_end) << endl;
     return 0;
 }
